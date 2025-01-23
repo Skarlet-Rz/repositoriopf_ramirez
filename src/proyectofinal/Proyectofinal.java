1e604197f51319ef351396c26d602ffe4984b95b
skarl <skarl@LAPTOP-GRO5T35V>
2025-01-22 23:51:29 -0500

    carga inicial

--- src/proyectofinal/Proyectofinal.java
+++ src/proyectofinal/Proyectofinal.java
@@ -1,20 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package proyectofinal;
-
-/**
- *
- * @author skarl
- */
+//paquete de datos e/s de datos por consola
+import java.util.Scanner;
+//clase principal
 public class Proyectofinal {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+//metodo principal
+public static void main(String[] args) {
+//declarar variables
+int vt1, vt2, vt3; 
+double sm, pt, c;
+String vendedor;
+Scanner lectura=new Scanner(System.in);
+//entrada de datos 
+System.out.print("Nombre del vendedor:");
+vendedor = lectura.next();
+System.out.print("Sueldo mensual:");
+sm = lectura.nextDouble();
+System.out.print("Venta1:");
+vt1 = lectura.nextInt();
+System.out.print("Venta2:");
+vt2 = lectura.nextInt();
+System.out.print("Venta 3:");
+vt3 = lectura.nextInt();
+//proceso de datos
+c=(vt1+vt2+vt3)*0.10;
+pt=sm+c;
+//salida de datos
+System.out.println("La comision de la venta es: "+c);
+System.out.println("El sueldo mensual del vendedor es: "+pt);
+}//fin del metodo
     
 }
